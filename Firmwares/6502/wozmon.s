ee1bfcff6c1ce86315acf7911c88e1741b0fd254
pdsilva <ppsilv@gmail.com>
2024-02-12 20:31:13 -0300

    inserindo diretorio Firmwares/6502

---
+++ Firmwares/6502/wozmon.s
@@ -0,0 +1,35 @@
+.setcpu "6502"
+.segment "WOZMON"
+
+;variaveis
+
+LEDON   =   $8000
+LEDOFF  =   $8001
+
+RESET:                           ;codigo
+                    sei
+;;A15 A14 A13 A12  | A11 A10 A09 A08
+;;    0      1      0     0   |     0     0      0      1
+;;    0      1      0     0   |     0     0      1      0
+LOOP:       
+                    LDA             #$00
+                    STA              $4100
+                    NOP
+                    NOP
+                    NOP
+                    NOP
+                    LDA          #$FF
+                    STA          $4200
+                    NOP
+                    NOP
+                    NOP
+                    NOP
+                    JMP   LOOP   
+                  
+                    
+;Cpu reset address
+.segment    "RESETVEC"
+
+                                        .word   $0F00
+                                        .word   RESET
+                                        .word   $0000
