f1601bc265bdc5a30b72a22108a483e3adbea1ec
pdsilva <ppsilv@gmail.com>
2024-02-17 19:32:54 -0300

    uartTest01.s delay at 1ms

--- Firmwares/UartTest/uartTest01.s
+++ Firmwares/UartTest/uartTest01.s
@@ -195,13 +195,15 @@ no_err:
 
 ;-----------------------------------------------------------------------------------------
 ; Delay 
+;cpu clock..: 1.843.200 hz
+;period.....: 0,542 ns
+;deley total: 1,0 ms 
 delay:
-    ldy  #$ff   ; (2 cycles)
-    ldx  #$ff   ; (2 cycles)
-delay1: dex         ; (2 cycles)
-    bne  delay1 ; (3 cycles in loop, 2 cycles at end)
-    dey         ; (2 cycles)
-    bne  delay1 ; (3 cycles in loop, 2 cycles at end)
+	ldx  #$CD   ; (2 cycles)
+delay1: 
+	nop			; (2 cycles) 1us
+	dex         ; (2 cycles) 1us
+	bne  delay1 ; (3 cycles in loop, 2 cycles at end) 1,5us in loop, 1us at end
 	rts
 
 ;/* --------------------------------------------------------------------------------------- 
